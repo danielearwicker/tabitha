630d06c0127e79f5ceda8612b3194efb81681e38
Daniel Earwicker <dan@earwicker.com>
2019-01-13 20:29:08 +0000

    Added build products

---
+++ docs/built/index.js
@@ -0,0 +1,27 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function tabulate(tabbedTable) {
+    var lines = tabbedTable.split("\n").map(function (l) { return l.split("\t"); });
+    function reduceCellLengths(line1, line2) {
+        var count = Math.max(line1.length, line2.length);
+        var result = new Array(count);
+        for (var i = 0; i < count; i++) {
+            result[i] = Math.max(line1[i] || 0, line2[i] || 0);
+        }
+        return result;
+    }
+    var cellLengths = lines.map(function (l) { return l.map(function (c) { return c.length; }); }).reduce(reduceCellLengths);
+    if (cellLengths.length === 0) {
+        return tabbedTable;
+    }
+    function padCells(line) {
+        return cellLengths.map(function (c, i) { return (line[i] || "").padEnd(c, " "); });
+    }
+    var _a = lines.map(function (l) { return padCells(l).join(" | "); }), header = _a[0], rows = _a.slice(1);
+    if (!rows || rows.length === 0) {
+        return header;
+    }
+    var divider = cellLengths.map(function (c) { return "-".repeat(c); }).join("-+-");
+    return [header, divider].concat(rows).join("\n");
+}
+exports.tabulate = tabulate;
---
+++ docs/built/index.test.js
@@ -0,0 +1,29 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var index_1 = require("./index");
+test("empty gives empty", function () {
+    expect(index_1.tabulate("")).toBe("");
+});
+test("tab becomes bar", function () {
+    expect(index_1.tabulate("a\tb")).toBe("a | b");
+});
+test("tabs become bars", function () {
+    expect(index_1.tabulate("very long\tshort\ttiny\treally long again"))
+        .toBe("very long | short | tiny | really long again");
+});
+test("multiple lines get a divider", function () {
+    expect(index_1.tabulate("First name\tLast name\tBorn\tNationality\n" +
+        "Obidiah\tMcTeabag\t1980\tScottish"))
+        .toBe("First name | Last name | Born | Nationality\n" +
+        "-----------+-----------+------+------------\n" +
+        "Obidiah    | McTeabag  | 1980 | Scottish   ");
+});
+test("columns are padded to constant width", function () {
+    expect(index_1.tabulate("First name\tLast name\tBorn\tNationality\n" +
+        "O\tMcTeabag\t1980\tScottish\n" +
+        "Melvin\tBogdanovich\t-\tEastern Basingstoke"))
+        .toBe("First name | Last name   | Born | Nationality        \n" +
+        "-----------+-------------+------+--------------------\n" +
+        "O          | McTeabag    | 1980 | Scottish           \n" +
+        "Melvin     | Bogdanovich | -    | Eastern Basingstoke");
+});
